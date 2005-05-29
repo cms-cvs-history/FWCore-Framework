d7030ada89b2b96712bcb03e07951c9db443359b
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-05-29 23:41:10 +0000

    correct file name in #include

--- test/stubs/eventsetup_dummy_stuff.cc
+++ test/stubs/eventsetup_dummy_stuff.cc
@@ -9,12 +9,12 @@
 //
 // Author:      Chris Jones
 // Created:     Thu May 26 16:17:26 EDT 2005
-// $Id: eventsetup_dummy_stuff.cc,v 1.1 2005/05/26 20:58:35 chrjones Exp $
+// $Id: eventsetup_dummy_stuff.cc,v 1.1 2005/05/29 02:29:54 wmtan Exp $
 //
 
 // system include files
 
 // user include files
-#include "FWCore/CoreFramework/test/EventSetup_DummyData.cc"
+#include "FWCore/CoreFramework/test/DummyData.cc"
 
 #include "FWCore/CoreFramework/test/DummyRecord.cc"
