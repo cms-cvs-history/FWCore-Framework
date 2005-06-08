a7a7699f8e5f49393ca4c81df5af304f0d4cf68e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-06-08 21:15:55 +0000

    implement GroupSelector for selecting output branches

--- test/EventProcessor2_t.cpp
+++ test/EventProcessor2_t.cpp
@@ -2,7 +2,7 @@
 
 Test of the EventProcessor class.
 
-$Id: EventProcessor2_t.cpp,v 1.1 2005/05/29 02:29:54 wmtan Exp $
+$Id: EventProcessor2_t.cpp,v 1.2 2005/06/02 20:20:00 wmtan Exp $
 
 ----------------------------------------------------------------------*/  
 #include <exception>
@@ -17,7 +17,7 @@ void work()
 		    "source = EmptyInputService { untracked int32 maxEvents = 5 }\n"
 		    "module m1 = IntProducer { int32 ivalue = 10 }\n"
 		    "module m2 = DoubleProducer { double dvalue = 3.3 }\n"
-		    "module out = AsciiOutputModule { int32 x = 1 }\n"
+		    "module out = AsciiOutputModule { untracked bool keepOnlySpecifiedProducts = false untracked vstring skipProducts = {} }\n"
                     "path p1 = { m1,m2,out }\n"
 		    "}\n");
   edm::EventProcessor proc(configuration);
