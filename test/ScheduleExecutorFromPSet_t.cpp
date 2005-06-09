b8d227de4f25d5d17d9261be8e4caf7ab47ad2a4
Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch>
2005-06-09 08:30:49 +0000

    Modified to test the schedule is executed in the right order by looking at the products of the modules. Currently the test fails. Use of boost_test is commented out to run the debugger more cleanly.

--- test/ScheduleExecutorFromPSet_t.cpp
+++ test/ScheduleExecutorFromPSet_t.cpp
@@ -3,13 +3,13 @@
    test for ScheduleExecutor
 
    \author Stefano ARGIRO
-   \version $Id: ScheduleExecutorFromPSet_t.cpp,v 1.3 2005/06/03 13:34:57 argiro Exp $
+   \version $Id: ScheduleExecutorFromPSet_t.cpp,v 1.4 2005/06/08 18:36:12 wmtan Exp $
    \date 18 May 2005
 */
 
-static const char CVSId[] = "$Id: ScheduleExecutorFromPSet_t.cpp,v 1.3 2005/06/03 13:34:57 argiro Exp $";
+static const char CVSId[] = "$Id: ScheduleExecutorFromPSet_t.cpp,v 1.4 2005/06/08 18:36:12 wmtan Exp $";
 
-#include "FWCore/CoreFramework/interface/EDProducer.h"
+
 #include "FWCore/CoreFramework/interface/ScheduleExecutor.h"
 #include "FWCore/CoreFramework/interface/ScheduleBuilder.h"
 #include "FWCore/CoreFramework/interface/UnknownModuleException.h"
@@ -28,18 +28,20 @@ static const char CVSId[] = "$Id: ScheduleExecutorFromPSet_t.cpp,v 1.3 2005/06/0
 #include "FWCore/ParameterSet/interface/Makers.h"
 #include "FWCore/ParameterSet/interface/MakeProcessPSet.h"
 
+#include "FWCore/CoreFramework/src/TypeID.h"
+
+#include "FWCore/CoreFramework/test/stubs/DummyProduct.h"
+
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <exception>
-#include "PluginManager/PluginManager.h"
 
-#include "boost/test/unit_test.hpp"
+
+//#include "boost/test/unit_test.hpp"
 
 using namespace edm;
 using namespace edm::pset;
 using namespace std;
-using namespace boost::unit_test_framework;
+//using namespace boost::unit_test_framework;
 
 auto_ptr<InputService> setupDummyInputService(){
 
@@ -67,11 +69,11 @@ const EventSetup& setupDummyEventSetup(){
 void test_one_path_with_sequence(){
 
   const char * conf =   "process test ={ \n"                  
-  "module a = TestSchedulerModule1 { uint32 a=1}\n"
-  "module b = TestSchedulerModule2 { uint32 a=2}\n"
-  "module c = TestSchedulerModule1 { uint32 a=3}\n"
-  "module d = TestSchedulerModule2 { uint32 a=4}\n"
-  "module e = TestSchedulerModule1 { uint32 a=5}\n" 
+  "module a = TestSchedulerModule1 { string module_name = \"a\" }\n"
+  "module b = TestSchedulerModule2 { string module_name = \"b\" }\n"
+  "module c = TestSchedulerModule1 { string module_name = \"c\" }\n"
+  "module d = TestSchedulerModule2 { string module_name = \"d\" }\n"
+  "module e = TestSchedulerModule1 { string module_name = \"e\" }\n" 
   "sequence s1 = { a,b}\n"
   "sequence s2 = { c,d}\n"
   "path p = { s1,s2,e}\n" 
@@ -80,8 +82,6 @@ void test_one_path_with_sequence(){
     
   boost::shared_ptr<edm::ParameterSet> processPSet= makeProcessPSet(conf);
   
-  // actual test of schedule executor
-  
   ScheduleBuilder builder(*processPSet);
   
   ScheduleExecutor executor(builder.getPathList());
@@ -89,19 +89,30 @@ void test_one_path_with_sequence(){
   auto_ptr<InputService> input = setupDummyInputService();
   auto_ptr<EventPrincipal> pep = input->readEvent();
   const EventSetup& c = setupDummyEventSetup();
-    
-  executor.runOneEvent(*pep,c);
-  
-}
 
+  executor.runOneEvent(*pep,c);
+
+  // this is only for testing and should never be done by the user
+//   Handle<EDProduct> tmp1 = 
+//     pep->getByLabel((TypeID(typeid(edmtest::DummyProduct))),string("a"));
+//   Handle<edmtest::DummyProduct> p1;
+//   convert_handle( tmp1,p1);
+
+  //  BOOST_CHECK(p1->getName()=="a");
+//   BOOST_CHECK(gProductCollector[1]=="b");
+//   BOOST_CHECK(gProductCollector[2]=="c");
+//   BOOST_CHECK(gProductCollector[3]=="d");
+//   BOOST_CHECK(gProductCollector[4]=="e");
+}
+/*
 void test_multiple_path_with_sequence(){
 
   const char * conf =   "process test ={ \n"                  
-  "module a = TestSchedulerModule1 { uint32 a=1}\n"
-  "module b = TestSchedulerModule2 { uint32 a=2}\n"
-  "module c = TestSchedulerModule1 { uint32 a=3}\n"
-  "module d = TestSchedulerModule2 { uint32 a=4}\n"
-  "module e = TestSchedulerModule1 { uint32 a=5}\n" 
+  "module a = TestSchedulerModule1 { string module_name = \"a\" }\n"
+  "module b = TestSchedulerModule2 { string module_name = \"b\" }\n"
+  "module c = TestSchedulerModule1 { string module_name = \"c\" }\n"
+  "module d = TestSchedulerModule2 { string module_name = \"d\" }\n"
+  "module e = TestSchedulerModule1 { string module_name = \"e\" }\n" 
   "sequence s1 = { a,b}\n"
   "sequence s2 = { c,d}\n"
   "path p1 = { s1,e}\n"
@@ -120,9 +131,17 @@ void test_multiple_path_with_sequence(){
   auto_ptr<InputService> input = setupDummyInputService();
   auto_ptr<EventPrincipal> pep = input->readEvent();
   const EventSetup& c = setupDummyEventSetup();
-  
-  executor.runOneEvent(*pep,c);
 
+ //  gProductCollector.clear();
+
+//   executor.runOneEvent(*pep,c); 
+
+//   BOOST_CHECK(gProductCollector[0]=="a"); 
+//   BOOST_CHECK(gProductCollector[1]=="b"); 
+//   BOOST_CHECK(gProductCollector[2]=="e");
+//   BOOST_CHECK(gProductCollector[3]=="c");
+//   BOOST_CHECK(gProductCollector[4]=="d");
+//   BOOST_CHECK(gProductCollector[5]=="e");
 }
 void test_failing_toload_module(){
 
@@ -146,14 +165,20 @@ const char * conf =   "process test ={ \n"
   BOOST_CHECK_THROW(ScheduleBuilder builder(*processPSet),UnknownModuleException);
 
 }
+*/
+int main(){
 
-test_suite*
-init_unit_test_suite( int /*argc*/, char* /*argv*/[] ) {
-    test_suite* test = BOOST_TEST_SUITE("TestScheduler");
+  test_one_path_with_sequence();
 
-    test->add( BOOST_TEST_CASE( &test_one_path_with_sequence ) );
-    test->add( BOOST_TEST_CASE( &test_multiple_path_with_sequence ) );
-    test->add( BOOST_TEST_CASE( &test_failing_toload_module ) );
-
-    return test;
 }
+
+// test_suite*
+// init_unit_test_suite( int /*argc*/, char* /*argv*/[] ) {
+//     test_suite* test = BOOST_TEST_SUITE("TestScheduler");
+
+//     test->add( BOOST_TEST_CASE( &test_one_path_with_sequence ) );
+//     test->add( BOOST_TEST_CASE( &test_multiple_path_with_sequence ) );
+//     test->add( BOOST_TEST_CASE( &test_failing_toload_module ) );
+
+//     return test;
+// }
