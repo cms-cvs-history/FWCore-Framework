b551c6a33287388f2e27732fbfee9850f2585e38
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-07-22 23:34:10 +0000

    remove warning messages and rule violations

--- test/stubs/TestInputService4ProductRegistry.cc
+++ test/stubs/TestInputService4ProductRegistry.cc
@@ -3,12 +3,13 @@
    test InputService for ProductRegistry
 
    \author Stefano ARGIRO
-   \version $Id$
+   \version $Id: TestInputService4ProductRegistry.cc,v 1.1 2005/07/21 21:07:14 argiro Exp $
    \date 21 Jul 2005
 */
 
-static const char CVSId[] = "$Id$";
+static const char CVSId[] = "$Id: TestInputService4ProductRegistry.cc,v 1.1 2005/07/21 21:07:14 argiro Exp $";
 
+#include <FWCore/Framework/interface/EventPrincipal.h>
 #include <FWCore/Framework/interface/InputService.h>
 #include <FWCore/Framework/interface/InputServiceDescription.h>
 #include <FWCore/Framework/interface/InputServiceMacros.h>
@@ -33,7 +34,7 @@ namespace edm {
       const edm::InputServiceDescription& desc): InputService(desc.process_name){
 
       const  edm::ProductRegistry::ProductList& plist=
-	desc.preg_->getProductList();
+	desc.preg_->productList();
       edm::ProductRegistry::ProductList::const_iterator pd = plist.begin();
 
       
@@ -56,7 +57,9 @@ namespace edm {
 
 
   private:
-    virtual std::auto_ptr<edm::EventPrincipal> read(){}
+    virtual std::auto_ptr<edm::EventPrincipal> read() {
+      return std::auto_ptr<edm::EventPrincipal>();
+    }
 
   
   }; 
