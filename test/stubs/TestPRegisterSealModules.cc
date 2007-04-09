44a53d80a9f065f3778c751b238f6e3050f01dbd
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2007-04-09 23:13:20 +0000

    updated to use new plugin manager

--- test/stubs/TestPRegisterSealModules.cc
+++ test/stubs/TestPRegisterSealModules.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Sat Sep 24 10:58:40 CEST 2005
-// $Id: TestPRegisterSealModules.cc,v 1.2 2005/12/28 00:51:29 wmtan Exp $
+// $Id: TestPRegisterSealModules.cc,v 1.3 2006/10/21 17:18:57 wmtan Exp $
 //
 
 // system include files
@@ -18,6 +18,5 @@
 #include "FWCore/Framework/test/stubs/TestPRegisterModule1.h"
 #include "FWCore/Framework/test/stubs/TestPRegisterModule2.h"
 
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(TestPRegisterModule1);
+DEFINE_FWK_MODULE(TestPRegisterModule1);
 DEFINE_ANOTHER_FWK_MODULE(TestPRegisterModule2);
