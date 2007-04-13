aa192e78afa313b4d7c92bb749f005132671914e
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-13 19:12:38 +0000

    Separate test libraries from plugins when necessary

--- test/DummySource.cc
+++ test/DummySource.cc
@@ -1,12 +1,11 @@
 /*----------------------------------------------------------------------
-$Id: DummySource.cc,v 1.4 2006/10/21 17:18:56 wmtan Exp $
+$Id: DummySource.cc,v 1.5 2006/12/13 22:15:22 wmtan Exp $
 ----------------------------------------------------------------------*/
 
 #include <stdexcept>
 #include <memory>
 
 #include "FWCore/Framework/test/DummySource.h"
-#include "FWCore/Framework/interface/InputSourceMacros.h"
 
 namespace edm {
   DummySource::DummySource(ParameterSet const& pset,
@@ -22,6 +21,3 @@ namespace edm {
     return true;
   }
 }
-using edm::DummySource;
-DEFINE_FWK_INPUT_SOURCE(DummySource);
-
